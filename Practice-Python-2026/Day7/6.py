e16a7643c6a136fea11dd3cdc99eb9658e06fc95
Asy1ai <157592314+Asy1ai@users.noreply.github.com>
2026-06-03 16:18:48 +0500

    Add files via upload

--- Practice-Python-2026/Day7/6.py
+++ Practice-Python-2026/Day7/6.py
@@ -1,7 +1,15 @@
-file = open("text.txt", "r", encoding="utf-8")
+try:
+    file = open("text.txt", "r", encoding="utf-8")
+    content = file.read()
+    print(content)
+    file.close()
 
-content = file.read()
+except FileNotFoundError:
+    file = open("text.txt", "w", encoding="utf-8")
+    file.write("Бұл файл автоматты түрде құрылды.")
+    file.close()
 
-print(content)
-
-file.close()
+    file = open("text.txt", "r", encoding="utf-8")
+    content = file.read()
+    print(content)
+    file.close()
